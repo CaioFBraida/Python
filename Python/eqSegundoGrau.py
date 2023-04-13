c40efad1e51f60dd4f4ce3df00f2503a118600f5
Caio Braida <caiobraida.p@gmail.com>
2023-04-13 11:34:18 -0300

    refatorando - funções

--- Python/eqSegundoGrau.py
+++ Python/eqSegundoGrau.py
@@ -2,23 +2,27 @@
 
 import math
 
+def delta(a, b, c):
+    return (b * b) - 4 * a * c
+
+
 a = int(input("Digite a "))
 b = int(input("Digite b "))
 c = int(input("Digite c "))
 
-delta = (b * b) - 4 * a * c
 
 
 
 
-if delta > 0:
+
+if delta(a,b,c) > 0:
     print("A equação possui 2 raízes.")
-    raiz1 = (-b + math.sqrt(delta) ) / 2 * a
-    raiz2 = (-b - math.sqrt(delta) ) / 2 * a
+    raiz1 = (-b + math.sqrt(delta(a,b,c)) ) / 2 * a
+    raiz2 = (-b - math.sqrt(delta(a,b,c)) ) / 2 * a
 
     print("primeira raiz = ", raiz1, " segunda raiz : ", raiz2)
 
-elif delta == 0:
+elif delta(a,b,c) == 0:
     print("A equação possui 1 raízes.")
     raiz = -b  / (2 * a)
 
