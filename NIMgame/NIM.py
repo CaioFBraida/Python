dd3a81afea1f058d32f909a6809b72ccdc79f072
Caio Braida <caiobraida.p@gmail.com>
2023-04-13 16:21:33 -0300

    inicio função partida

--- NIMgame/NIM.py
+++ NIMgame/NIM.py
@@ -1,37 +1,49 @@
 def computador_escolhe_jogada(n, m):
-    
-    #n - numParaRetirar == numQueVaiFicar
     i = n
-    while(i > 0):
-        if(i % (m + 1) == 0):
+    while (i > 0):
+        if (i % (m + 1) == 0):
             numQueVaiFicar = i
             break
         i = i - 1
-        
-    if(numQueVaiFicar % (m + 1) == 0):
+
+    if (numQueVaiFicar % (m + 1) == 0):
         numParaRetirar = n - numQueVaiFicar
     else:
         numParaRetirar = m
 
-
     return numParaRetirar
+
 
 def usuario_escolhe_jogada(n, m):
     naoAcertou = True
     valorJogadoUsuario = int(input("Quantas peças você vai tirar? "))
-    while(naoAcertou):
-        if(valorJogadoUsuario <= m and valorJogadoUsuario > 0):
+    while (naoAcertou):
+        if (valorJogadoUsuario <= m and valorJogadoUsuario > 0):
             naoAcertou = False
         else:
             print("Oops! Jogada inválida! Tente de novo.")
             valorJogadoUsuario = int(input("Quantas peças você vai tirar? "))
     return valorJogadoUsuario
 
-
-def main():
+def partida():
     n = int(input("Digite o número de peças "))
     m = int(input("Digite o número max de peças a serem retiradas "))
-    print(usuario_escolhe_jogada(n, m))
+    if(n % (m+1) == 0):
+        print("Você começa!")
+        while():
+            computador_escolhe_jogada(n, m)
+            usuario_escolhe_jogada(n, m)
+    else:
+        print("Computador começa!")
+        while():
+            usuario_escolhe_jogada(n, m)
+            computador_escolhe_jogada(n, m)
+        
+    #atualiza n
+    #identifica que jogo acabou
+
+
+def main():
+    partida()
 
 main()
-
