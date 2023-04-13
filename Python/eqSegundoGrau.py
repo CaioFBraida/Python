fbb1f45d5c3ce9679d1f443576f4a275f1e29892
Caio Braida <caiobraida.p@gmail.com>
2023-04-13 11:49:10 -0300

    refatorando em mais funçoes

--- Python/eqSegundoGrau.py
+++ Python/eqSegundoGrau.py
@@ -1,32 +1,31 @@
-#PROGRAMA  calcula valor de raizes equação segundo grau
+# PROGRAMA  calcula valor de raizes equação segundo grau
 
 import math
+
+
 
 def delta(a, b, c):
     return (b * b) - 4 * a * c
 
 
-a = int(input("Digite a "))
-b = int(input("Digite b "))
-c = int(input("Digite c "))
+def main():
+    a = int(input("Digite a "))
+    b = int(input("Digite b "))
+    c = int(input("Digite c "))
+    imprime_raizes(a, b, c)
 
 
+def imprime_raizes(a, b, c):
+    if delta(a, b, c) > 0:
+        print("A equação possui 2 raízes.")
+        raiz1 = (-b + math.sqrt(delta(a, b, c))) / 2 * a
+        raiz2 = (-b - math.sqrt(delta(a, b, c))) / 2 * a
+        print("primeira raiz = ", raiz1, " segunda raiz : ", raiz2)
 
+    elif delta(a, b, c) == 0:
+        print("A equação possui 1 raízes.")
+        raiz = -b / (2 * a)
+        print("A raiz vale : ", raiz)
 
-
-
-if delta(a,b,c) > 0:
-    print("A equação possui 2 raízes.")
-    raiz1 = (-b + math.sqrt(delta(a,b,c)) ) / 2 * a
-    raiz2 = (-b - math.sqrt(delta(a,b,c)) ) / 2 * a
-
-    print("primeira raiz = ", raiz1, " segunda raiz : ", raiz2)
-
-elif delta(a,b,c) == 0:
-    print("A equação possui 1 raízes.")
-    raiz = -b  / (2 * a)
-
-    print("A raiz vale : ", raiz)
-
-else:
-    print("A equação não possuiu raizes")
+    else:
+        print("A equação não possuiu raizes")
