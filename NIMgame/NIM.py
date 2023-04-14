b01070b35e26039f1099c60b8f9fe412f9b7382c
Caio Braida <caiobraida.p@gmail.com>
2023-04-14 15:00:55 -0300

    corrigindo função computador escolhe - acertando funçao partida

--- NIMgame/NIM.py
+++ NIMgame/NIM.py
@@ -1,12 +1,13 @@
 def computador_escolhe_jogada(n, m):
     i = n
+    numQueVaiFicar = -100
     while (i > 0):
         if (i % (m + 1) == 0):
             numQueVaiFicar = i
             break
         i = i - 1
 
-    if (numQueVaiFicar % (m + 1) == 0):
+    if (numQueVaiFicar % (m + 1) == 0 and numQueVaiFicar != (-100)):
         numParaRetirar = n - numQueVaiFicar
     else:
         numParaRetirar = m
@@ -34,41 +35,63 @@ def partida():
             usuarioVaiTirar = usuario_escolhe_jogada(n, m)
             n = n - usuarioVaiTirar
             if(usuarioVaiTirar > 1):
-                print("Você tirou " + usuarioVaiTirar + " peças.")
+                print("Você tirou " + str(usuarioVaiTirar) + " peças.")
             else:
                 print("Você tirou uma peça.")
-
             if(n > 1):
-                print("Agora restam " + n + " peças no tabuleiro.")
+                print("Agora restam " + str(n) + " peças no tabuleiro.")
             else:
                 print("Agora resta apenas uma peça no tabuleiro.")
 
+
             compVaiTirar = computador_escolhe_jogada(n, m)
             n = n - compVaiTirar
-
             if(compVaiTirar > 1):
-                print("O computador tirou " + compVaiTirar + " peças.")
+                print("O computador tirou " + str(compVaiTirar) + " peças.")
             else:
                 print("O computador tirou uma peça.")
 
             if(n > 1):
-                print("Agora restam " + n + " peças no tabuleiro.")
+                print("Agora restam " + str(n) + " peças no tabuleiro.")
             else:
                 print("Agora resta apenas uma peça no tabuleiro.")
+
+        print("O computador ganhou!")
             
-
-
     else:
         print("Computador começa!")
         while(n > 0):
-            n - computador_escolhe_jogada(n, m)
-            n - usuario_escolhe_jogada(n, m)
-        
-    #atualiza n
-    #identifica que jogo acabou
+            compVaiTirar = computador_escolhe_jogada(n, m)
+            n = n - compVaiTirar
+            if(compVaiTirar > 1):
+                print("O computador tirou " + str(compVaiTirar) + " peças.")
+            else:
+                print("O computador tirou uma peça.")
+
+            if(n > 1):
+                print("Agora restam " + str(n) + " peças no tabuleiro.")
+            else:
+                print("Agora resta apenas uma peça no tabuleiro.")
+
+            usuarioVaiTirar = usuario_escolhe_jogada(n, m)
+            n = n - usuarioVaiTirar
+            if(usuarioVaiTirar > 1):
+                print("Você tirou " + str(usuarioVaiTirar) + " peças.")
+            else:
+                print("Você tirou uma peça.")
+            if(n > 1):
+                print("Agora restam " + str(n) + " peças no tabuleiro.")
+            else:
+                print("Agora resta apenas uma peça no tabuleiro.")
+
+        print("O computador ganhou!")
+
 
 
 def main():
-    partida()
+   # n = int(input("Digite o número de peças "))
+   # m = int(input("Digite o número max de peças a serem retiradas "))
+   # print(computador_escolhe_jogada(n, m))
+   partida()
 
 main()
