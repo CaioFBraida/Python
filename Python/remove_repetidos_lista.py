f050310d571089ff0137a75d19af58c1802620b9
Caio Braida <caiobraida.p@gmail.com>
2023-04-18 15:25:17 -0300

    exercício de listas, ordenação e remoção

---
+++ Python/remove_repetidos_lista.py
@@ -0,0 +1,9 @@
+def remove_repetidos(lista):
+    lista_removida = []
+    for i in range(0,len(lista)-1):
+        if(lista[i] not in lista_removida):
+            lista_removida.append (lista[i])
+
+    lista_removida.sort()
+    return lista_removida
+
