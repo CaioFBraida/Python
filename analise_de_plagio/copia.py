6ffad9622274bf0aea1d66871ce60f1a3ef8a5ef
Caio Braida <caiobraida.p@gmail.com>
2023-05-02 15:23:27 -0300

    funçaõ avalia_textos completa

--- analise_de_plagio/copia.py
+++ analise_de_plagio/copia.py
@@ -2,9 +2,6 @@
 ##ESQUELETO DE CÓDIGO PRONTO - 7 PRIMEIRAS FUNÇÃO FEITAS - TODAS AS OUTRAS AUTORIA PRÓPRIA.
 ##concertar funçoes tamanho medio sentença e tamanho medio frase
 
-# organizar função avalia textos - enviando o numero errado
-# organizar funçoes tam medio sentença e frase -> testar pelo codigo no site 
-
 import re
 
 def le_assinatura():
@@ -135,13 +132,12 @@ def calcula_tamanho_medio_frase(lista_frases_texto_unica):
     lista_numero_caracteres_frase = []
     for i in range(len(lista_frases_texto_unica)):
         lista_numero_caracteres_frase.append (len(lista_frases_texto_unica[i]))
-    print(lista_numero_caracteres_frase)
 
     for numero in lista_numero_caracteres_frase:
         soma_caracteres_frase = soma_caracteres_frase + numero
 
     tamanho_medio_frase = soma_caracteres_frase / len(lista_frases_texto_unica)
-    print(tamanho_medio_frase)
+  
     return tamanho_medio_frase
 
 def compara_assinatura(as_a, as_b):
@@ -208,45 +204,38 @@ def calcula_assinatura(texto):
 
 def avalia_textos(textos, ass_cp):
     '''IMPLEMENTAR. Essa funcao recebe uma lista de textos e uma assinatura ass_cp e deve devolver o numero (1 a n) do texto com maior probabilidade de ter sido infectado por COH-PIAH.'''
+
+    lista_assinaturas_texto = []
+    for i in range(len(textos)):
+        lista_assinaturas_texto.append(calcula_assinatura(textos[i]))
+
+    lista_grau_similaridade_textos = []
+    for i in range(len(lista_assinaturas_texto)):
+        lista_grau_similaridade_textos.append(compara_assinatura(lista_assinaturas_texto[i], ass_cp))
     
-    grau_similaridade_textos = []
-    for texto in (textos):
-        ass_texto = []
-        ass_texto = calcula_assinatura(texto)
-        grau_similaridade_textos.append(compara_assinatura(ass_cp, ass_texto))
-
-    menor_numero = grau_similaridade_textos[0]
+    valor_do_texto_infectado = lista_grau_similaridade_textos[0]
     numero_do_texto_infectado = 0
-    for i in range(len(grau_similaridade_textos)):
-        if grau_similaridade_textos[i] < menor_numero:
-            numero_do_texto_infectado = i 
-
+    for i in range(len(lista_grau_similaridade_textos)):
+        if valor_do_texto_infectado > lista_grau_similaridade_textos[i]:
+            valor_do_texto_infectado = lista_grau_similaridade_textos[i]
+            numero_do_texto_infectado = i + 1
+    
     return numero_do_texto_infectado 
 
 
 def main():
-    ######### testes aqui
-    texto = "Então resolveu ir brincar com a Máquina pra ser também imperador dos filhos da mandioca. Mas as três cunhas deram muitas risadas e falaram que isso de deuses era gorda mentira antiga, que não tinha deus não e que com a máquina ninguém não brinca porque ela mata. A máquina não era deus não, nem possuía os distintivos femininos de que o herói gostava tanto. Era feita pelos homens. Se mexia com eletricidade com fogo com água com vento com fumo, os homens aproveitando as forças da natureza. Porém jacaré acreditou? nem o herói! Se levantou na cama e com um gesto, esse sim! bem guaçu de desdém, tó! batendo o antebraço esquerdo dentro do outro dobrado, mexeu com energia a munheca direita pras três cunhas e partiu. Nesse instante, falam, ele inventou o gesto famanado de ofensa: a pacova."
+    ######### testes aqui:
     
-    ass = calcula_assinatura(texto)
-    print(ass)
-    print()
-    
-
     #############
+    assinatura_base = []
+    assinatura_base = le_assinatura()
+    listas_de_textos_lidos = []
+    listas_de_textos_lidos = le_textos()
 
-
-    #assinatura_base = []
-    #assinatura_base = le_assinatura()
-    #listas_de_textos_lidos = []
-    #listas_de_textos_lidos = le_textos()
-
-    #listas_de_textos_lidos = ["Num fabulário ainda por encontrar será um dia lida esta fábula: A uma bordadora dum país longínquo foi encomendado pela sua rainha que bordasse, sobre seda ou cetim, entre folhas, uma rosa branca. A bordadora, como era muito jovem, foi procurar por toda a parte aquela rosa branca perfeitíssima, em cuja semelhança bordasse a sua. Mas sucedia que umas rosas eram menos belas do que lhe convinha, e que outras não eram brancas como deviam ser. Gastou dias sobre dias, chorosas horas, buscando a rosa que imitasse com seda, e, como nos países longínquos nunca deixa de haver pena de morte, ela sabia bem que, pelas leis dos contos como este, não podiam deixar de a matar se ela não bordasse a rosa branca. Por fim, não tendo melhor remédio, bordou de memória a rosa que lhe haviam exigido. Depois de a bordar foi compará-la com as rosas brancas que existem realmente nas roseiras. Sucedeu que todas as rosas brancas se pareciam exactamente com a rosa que ela bordara, que cada uma delas era exactamente aquela. Ela levou o trabalho ao palácio e é de supor que casasse com o príncipe. No fabulário, onde vem, esta fábula não traz moralidade. Mesmo porque, na idade de ouro, as fábulas não tinham moralidade nenhuma.", "Voltei-me para ela; Capitu tinha os olhos no chão. Ergueu-os logo, devagar, e ficamos a olhar um para o outro... Confissão de crianças, tu valias bem duas ou três páginas, mas quero ser poupado. Em verdade, não falamos nada; o muro falou por nós. Não nos movemos, as mãos é que se estenderam pouco a pouco, todas quatro, pegando-se, apertando-se, fundindo-se. Não marquei a hora exata daquele gesto. Devia tê-la marcado; sinto a falta de uma nota escrita naquela mesma noite, e que eu poria aqui com os erros de ortografia que trouxesse, mas não traria nenhum, tal era a diferença entre o estudante e o adolescente. Conhecia as regras do escrever, sem suspeitar as do amar; tinha orgias de latim e era virgem de mulheres.", "Senão quando, estando eu ocupado em preparar e apurar a minha invenção, recebi em cheio um golpe de ar; adoeci logo, e não me tratei. Tinha o emplasto no cérebro; trazia comigo a idéia fixa dos doidos e dos fortes. Via-me, ao longe, ascender do chão das turbas, e remontar ao Céu, como uma águia imortal, e não é diante de tão excelso espetáculo que um homem pode sentir a dor que o punge. No outro dia estava pior; tratei-me enfim, mas incompletamente, sem método, nem cuidado, nem persistência; tal foi a origem do mal que me trouxe à eternidade. Sabem já que morri numa sexta-feira, dia aziago, e creio haver provado que foi a minha invenção que me matou. Há demonstrações menos lúcidas e não menos triunfantes. Não era impossível, entretanto, que eu chegasse a galgar o cimo de um século, e a figurar nas folhas públicas, entre macróbios. Tinha saúde e robustez. Suponha-se que, em vez de estar lançando os alicerces de uma invenção farmacêutica, tratava de coligir os elementos de uma instituição política, ou de uma reforma religiosa. Vinha a corrente de ar, que vence em eficácia o cálculo humano, e lá se ia tudo. Assim corre a sorte dos homens."]
-    #assinatura_base = [4.51, 0.693, 0.55, 70.82, 1.82, 38.5]
-    #numero_texto_infectado = avalia_textos(listas_de_textos_lidos, assinatura_base)
+    numero_texto_infectado = avalia_textos(listas_de_textos_lidos, assinatura_base)
     
+    print()
+    print("O autor do texto " +  str(numero_texto_infectado) +  " está infectado com COH-PIAH")
 
-    #print("O autor do texto " +  str(numero_texto_infectado) +  " está infectado com COH-PIAH")
 
-main()  
 
